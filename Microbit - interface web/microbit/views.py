01f2e9f287ed16978b4ba9cd9946c11d274a5bb4
gbusquim <gbtatu@hotmail.com>
2019-06-19 14:14:54 -0300

    views certo

--- Microbit - interface web/microbit/views.py
+++ Microbit - interface web/microbit/views.py
@@ -1,21 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from django.views.decorators.csrf import csrf_protect
 import serial
 import random
 
 
-serialPort = None
-# Create your views here.
-def home(request):
+serialPort = serial.Serial('com3',115200,timeout = 0.25)
+serialPort.close()
+# Create your views here
+
+def Home(request):
+    return render(request,'jogo.html')
+
+def Terminal(request):
+    return render(request,'inicial.html')
+
+def iFrame(request):
+    #if serialPort.isOpen
+    return render(request,'iFrame.html')
+
+def ComSerial(request):
     global serialPort
-    serialPort = serial.Serial('com3',115200,timeout=1)
-    return render(request,'microbit/home.html')
+    #serialPort = serial.Serial('com3',115200,timeout=1)
+    return render(request,'home.html')
 
 def trataRequest(request):
-    valor = serialPort.read(1).decode("utf-8")
+    valor = ''
+    if not serialPort.isOpen():
+        serialPort.open()
+        valor = serialPort.read(1).decode("utf-8")
+        #valor = serialPort.read(4).decode("utf-8")
     #a = random.randint(1,101)
-    print(valor)
+        print(valor)
+        serialPort.close()
     return HttpResponse(valor)
+
+def EscreveRequest(request):
+    if not serialPort.isOpen():
+        serialPort.open()
+    valor = request.POST.get("draw")
+    print(valor)
+    serialPort.write(valor.encode('utf-8'))
+#a = random.randint(1,101)
+    serialPort.close()
+    return HttpResponse("success")
+
 
 def fechaPorta(request):
     #serialPort.close()
