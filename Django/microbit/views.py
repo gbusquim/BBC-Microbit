0af54aa230ea839b035dffedc62b0283ecb3f4ea
gbusquim <gbtatu@hotmail.com>
2019-09-02 22:13:52 -0300

    inclusao de imagem na tela

--- Django/microbit/views.py
+++ Django/microbit/views.py
@@ -16,12 +16,10 @@ def Terminal(request):
     return render(request,'inicial.html')
 
 def iFrame(request):
-    #if serialPort.isOpen
     return render(request,'iFrame.html')
 
 def ComSerial(request):
     global serialPort
-    #serialPort = serial.Serial('com3',115200,timeout=1)
     return render(request,'home.html')
 
 def trataRequest(request):
@@ -29,8 +27,6 @@ def trataRequest(request):
     if not serialPort.isOpen():
         serialPort.open()
         valor = serialPort.read(1).decode("utf-8")
-        #valor = serialPort.read(4).decode("utf-8")
-    #a = random.randint(1,101)
         print(valor)
         serialPort.close()
     return HttpResponse(valor)
@@ -40,31 +36,12 @@ def EscreveRequest(request):
         serialPort.open()
     valor = request.POST.get("message")
     serialPort.write(valor.encode('utf-8'))
-#a = random.randint(1,101)
     serialPort.close()
     return HttpResponse("success")
 
 
 def fechaPorta(request):
-    #serialPort.close()
     print(serialPort.isOpen())
     return HttpResponse(1)
 
 
-#Verifica se a porta esta aberta
-#serialPort.isOpen()
-
-#Le da porta -> passa numero de bytes a serem lidos como parametro
-
-
-#print(valor.decode("utf-8"))
-
-
-
-
-#serialPort.open()
-
-
-
-
-#serialPort.close()
